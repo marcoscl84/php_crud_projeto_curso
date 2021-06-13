b49e354a0b4a0a1dec9c30572b4754bc88b4d922
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2021-06-12 22:14:29 -0300

    botão e página de edição

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ include_once 'includes/message.php';
                     <td><?php echo $dados['sobrenome']; ?><td>
                     <td><?php echo $dados['email']; ?><td>
                     <td><?php echo $dados['idade']; ?><td>
-                    <td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a><td>
+                    <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a><td>
                     <td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a><td>
                 </tr>
                 <?php endwhile; ?>
